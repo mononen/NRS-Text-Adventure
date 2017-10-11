e21a7f84c37904f1310c72c274853af093c96691
Alex <almo121@live.com>
2017-10-11 13:12:41 -0700

    Began fiddling around with SWT and window generation

---
+++ src/MainWnd.java
@@ -0,0 +1,69 @@
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.*;
+import org.eclipse.swt.graphics.*;
+import org.eclipse.swt.layout.*;
+import org.eclipse.swt.widgets.*;
+
+public class MainWnd
+{
+	private static Label label2;
+	private static Shell shell;
+	public static void main(String[] args)
+	{
+		Display display = new Display();
+		shell = new Shell(display);
+		//shell.setLayout(new FillLayout());
+		
+		label2 = new Label(shell, SWT.NONE);
+		Font font = new Font(label2.getDisplay(), new FontData("Mono", 10, SWT.ITALIC));
+		label2.setFont(font);
+		label2.setText("here's some text with a font applied");
+		label2.setBounds(0, 0, 800, 50);
+		
+		Label label = new Label(shell, SWT.BORDER);
+		label.setText("this is a label:");
+		label.setToolTipText("this is a label tooltip");
+		//user can type in the text widget
+		label.setBounds(0, 50, 800, 50);
+		Text text = new Text(shell, SWT.NONE);
+		text.setText("this is the text in the text widget");
+		text.setBackground(display.getSystemColor(SWT.COLOR_DARK_BLUE));
+		text.setForeground(display.getSystemColor(SWT.COLOR_CYAN));
+		text.setBounds(0, 160, 800, 100);
+		
+		int toolbarSize = 30;
+		
+		
+		Button button = new Button(shell, SWT.PUSH);
+		button.setBounds(50, 100, 80, 60);
+		button.addSelectionListener(new SelectionAdapter()
+			{
+				@Override
+				public void widgetSelected(SelectionEvent e)
+				{
+					System.out.println(text.getText());
+				}
+			});
+		shell.setBounds(100, 100, 800, 600);
+		shell.open();
+		shell.addMouseMoveListener(e -> showSize(e));
+		label2.addMouseMoveListener(e -> showSize(e));
+		while (!shell.isDisposed())
+		{
+			if(!display.readAndDispatch())
+			{
+				display.sleep();
+			}
+		}
+		display.dispose();
+	}
+	public static void showSize(MouseEvent e)
+	{
+		int x = e.x;
+		int y = e.y;
+		String s = "bounds for label: " + label2.getBounds() + "\n";
+		s+= "bounds for shell: " + shell.getBounds() + "\n";
+		s+= "mouse pointer: " + x + " " + y;
+		label2.setText(s);
+	}
+}
